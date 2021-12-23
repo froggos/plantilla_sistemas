99bc3a07c6469ef77efd6d7f24234a24242f0a90
sapitolindo <benja.v.o@hotmail.com>
2021-12-23 16:58:20 -0300

    Sidebar y navbar finalizada

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -1,8 +1,33 @@
 let toggler = document.querySelector(".sidebar-toggler");
-toggler.addEventListener("click", function(){
+
+let hidden_toggler = document.getElementById("btn-toggler");
+
+hidden_toggler.addEventListener("click", function () {
+    toggle();
+});
+
+toggler.addEventListener("click", function () {
     toggle();
 });
 
 const toggle = () => {
     document.querySelector("body").classList.toggle("active");
 }
+
+let lastState = false;
+const checkChange = () => {
+    let body = document.querySelector("body");
+    let state = window.matchMedia("(max-width: 768px)").matches;
+    if (state != lastState) {
+        if (state) {
+            body.classList.toggle("active");
+        } else {
+            if (body.classList.contains("active")) {
+                document.querySelector("body").classList.toggle("active");
+            }
+        }
+        lastState = state;
+    }
+}
+
+window.setInterval(checkChange, 150);
